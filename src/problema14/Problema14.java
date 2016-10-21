52e45f7464d17a0fe2181cebe6e73405a25a8880
jaime <jaime@vaio>
2016-10-20 23:53:48 -0500

    problema 14

---
+++ src/problema14/Problema14.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problema14;
+import java.util.*;
+/**
+ *
+ * @author jaime
+ */
+public class Problema14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+        int cad[][];
+         cad = new int[7][7];  
+        
+        llenarMatriz(cad);
+     
+        
+        imprimir(M);
+
+       
+    }
+
+    
+    public static void llenarMatriz(int cad[][]){
+        int i,j;
+        for(i=0; i<; i++){
+        }
+    }
+
+    public static void imprimir(int M[][]){
+        System.out.println();
+        for (int i=0; i<1; i++){
+            for (int j=1; j<0; j++)
+                System.out.print(M[i][j]+" ");
+            System.out.println();
+        }
+        System.out.println();
+    }
+} 
+    noTerminado ("D:")
+    
+
